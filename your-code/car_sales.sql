891e0b0182906d7748a987842065b0788245ac68
Jose Marin <josmep@gmail.com>
2023-04-25 23:43:15 +0200

    just in time!

--- your-code/car_sales.sql
+++ your-code/car_sales.sql
@@ -6,67 +6,73 @@ CREATE SCHEMA IF NOT EXISTS `car_sales` DEFAULT CHARACTER SET utf8mb4 COLLATE ut
 USE `car_sales` ;
 
 CREATE TABLE IF NOT EXISTS `car_sales`.`cars` (
-  `ID` INT NOT NULL,
+  `ID` INT(10) NOT NULL,
   `VIN` VARCHAR(17) NOT NULL,
   `Manufacturer` VARCHAR(20) NULL DEFAULT NULL,
-  `Car_model` VARCHAR(20) NOT NULL,
-  `Date` YEAR NOT NULL,
+  `Model` VARCHAR(20) NOT NULL,
+  `Year` YEAR(4) NULL,
   `Color` VARCHAR(20) NULL DEFAULT NULL,
-  PRIMARY KEY (`ID`, `VIN`),
+  PRIMARY KEY (`ID`),
   UNIQUE INDEX `VIN_UNIQUE` (`VIN` ASC) VISIBLE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `car_sales`.`customers` (
+  `ID` INT(10) NOT NULL,
   `Customer ID` INT NOT NULL,
-  `c_name` VARCHAR(20) NOT NULL,
-  `s_surname` VARCHAR(20) NOT NULL,
-  `Phone number` VARCHAR(30) NOT NULL,
-  `Email` VARCHAR(30) NOT NULL,
+  `Name` VARCHAR(20) NOT NULL,
+  `Phone` VARCHAR(30) NOT NULL,
+  `Email` VARCHAR(30) NULL,
   `Adress` VARCHAR(45) NULL DEFAULT NULL,
   `City` VARCHAR(45) NULL DEFAULT NULL,
-  `state/province` VARCHAR(45) NULL DEFAULT NULL,
+  `State/Province` VARCHAR(45) NULL DEFAULT NULL,
   `Country` VARCHAR(20) NULL DEFAULT NULL,
-  `Zip/postal code` VARCHAR(10) NULL DEFAULT NULL,
-  PRIMARY KEY (`Customer ID`))
+  `Postal` VARCHAR(10) NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `car_sales`.`salespersons` (
-  `Staff ID` INT NOT NULL,
-  `sp_name` VARCHAR(45) NOT NULL,
-  `sp_surname` VARCHAR(45) NOT NULL,
+  `ID` INT(10) NOT NULL,
+  `Staff ID` INT(10) NOT NULL,
+  `Name` VARCHAR(45) NOT NULL,
   `Store` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`Staff ID`))
+  PRIMARY KEY (`ID`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `car_sales`.`invoices` (
-  `ID` VARCHAR(10) NULL,
+  `ID` INT(10) NULL,
   `Invoice number` INT NOT NULL,
-  `Invoice_date` DATE NOT NULL,
-  `Customers_Customer ID` INT NOT NULL,
-  `Cars_VIN` VARCHAR(17) NOT NULL,
-  `Salespersons_Staff ID` INT NOT NULL,
-  PRIMARY KEY (`Customers_Customer ID`, `Cars_VIN`, `Salespersons_Staff ID`),
-  INDEX `fk_Invoices_Customers_idx` (`Customers_Customer ID` ASC) VISIBLE,
-  INDEX `fk_Invoices_Cars1_idx` (`Cars_VIN` ASC) VISIBLE,
-  INDEX `fk_Invoices_Salespersons1_idx` (`Salespersons_Staff ID` ASC) VISIBLE,
-  CONSTRAINT `fk_Invoices_Cars1`
-    FOREIGN KEY (`Cars_VIN`)
-    REFERENCES `car_sales`.`cars` (`VIN`),
-  CONSTRAINT `fk_Invoices_Customers`
-    FOREIGN KEY (`Customers_Customer ID`)
-    REFERENCES `car_sales`.`customers` (`Customer ID`),
-  CONSTRAINT `fk_Invoices_Salespersons1`
-    FOREIGN KEY (`Salespersons_Staff ID`)
-    REFERENCES `car_sales`.`salespersons` (`Staff ID`))
+  `Date` DATE NOT NULL,
+  `cars_ID` INT(10) NOT NULL,
+  `customers_ID` INT(10) NOT NULL,
+  `salespersons_ID` INT(10) NOT NULL,
+  PRIMARY KEY (`cars_ID`, `customers_ID`, `salespersons_ID`),
+  INDEX `fk_invoices_salespersons1_idx` (`salespersons_ID` ASC) VISIBLE,
+  INDEX `fk_invoices_customers1_idx` (`customers_ID` ASC) VISIBLE,
+  CONSTRAINT `fk_invoices_cars`
+    FOREIGN KEY (`cars_ID`)
+    REFERENCES `car_sales`.`cars` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_invoices_salespersons1`
+    FOREIGN KEY (`salespersons_ID`)
+    REFERENCES `car_sales`.`salespersons` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_invoices_customers1`
+    FOREIGN KEY (`customers_ID`)
+    REFERENCES `car_sales`.`customers` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
